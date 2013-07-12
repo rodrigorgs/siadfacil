844fcbd30ea2c9cb17f6871f65d5e072aaed562e
rodrigorgs <rodrigorgs@gmail.com>
2013-07-12 16:30:35 -0300

    Obtém nome do interessado a partir do numero.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'mechanize'
+gem 'logger'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,25 @@
 class HomeController < ApplicationController
   def index
+    num = "#{params['numero']}"
     
+    if (num)
+      agent = Mechanize.new
+      agent.log = Logger.new "mech.log"
+      agent.user_agent_alias = 'Mac Safari'
+      
+      page = agent.get "http://www.siad.ufba.br/login.asp"
+      search_form = page.form_with :name => "frmConsultaProcesso"
+      search_form.field_with(:name => "numero_processo").value = num
+  
+      search_results = agent.submit search_form
+      
+      #content_type 'text/html', :charset => 'windows-1252'
+      
+      body = search_results.body
+      
+      if body =~ %r{Nome\s*Interessado</font></b></td>\s*<td width="84%" bgcolor="#c0c0c0"><font face="Verdana, Arial, Helvetica" size="1">(.*?)</font>}
+        @resultado = "Nome do interessado: #{$1}"
+      end
+    end
   end
 end
