09ce99587241e7ca25f8da666d4e0a9eeeb5f450
rodrigorgs <rodrigorgs@gmail.com>
2013-07-12 15:39:44 -0300

    Versão inicial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Siadfacil::Application.config.secret_key_base = '4c697bec7dbf384b6263655e88349663ebe6eeb96df32eb43ba8bab5ceeda3f9b7a35423730886c1017a0400649a70285f35768dab76affdacd946b8e7075c09'
